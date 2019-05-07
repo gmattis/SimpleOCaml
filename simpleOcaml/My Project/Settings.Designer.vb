1a84a9d6a54808ed55bc4559eb8e521fcfbacf0c
gmattis <mattis.golliet@laposte.net>
2019-05-07 13:59:49 +0200

    Version update

--- simpleOcaml/My Project/Settings.Designer.vb
+++ simpleOcaml/My Project/Settings.Designer.vb
@@ -176,7 +176,7 @@ Namespace My
         
         <Global.System.Configuration.UserScopedSettingAttribute(),  _
          Global.System.Diagnostics.DebuggerNonUserCodeAttribute(),  _
-         Global.System.Configuration.DefaultSettingValueAttribute("v1.1")>  _
+         Global.System.Configuration.DefaultSettingValueAttribute("v1.1.5")>  _
         Public Property Version() As String
             Get
                 Return CType(Me("Version"),String)
